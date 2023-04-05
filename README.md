ac316bc1e981379e9227c47472abea39a41edd7a
omnipede <omnipede@naver.com>
2023-04-05 15:37:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# myzsh
+
+zsh 설정 스크립트
+
+# 실행 방법
+
+* 스크립트 실행
+
+```sh
+sh -c "$(curl -fsSL )
+```
+
+* `~/.zshrc` 에 `plugins` 필드에 다음 코드 삽입
+
+```sh
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+```
+
+* `~/.zshrc` 반영
+
+```sh
+$ source ~/.zshrc
+```
---
+++ build_ubuntu.sh
@@ -0,0 +1,9 @@
+# zsh 설치
+sudo apt-get install zsh
+
+# oh-my-zsh 설치
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# 커스텀 플러그인 설치
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
